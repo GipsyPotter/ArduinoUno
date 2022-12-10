8c022013951238407b5887049d5617ae1de7fa19
ThePotter <nguyentadac@gmail.com>
2022-12-10 08:56:49 +0700

    1

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,13 @@
 #include <Arduino.h>
 void setup() {
-// write your initialization code here
+    // put your setup code here, to run once:
+    pinMode(13, OUTPUT);
 }
 
 void loop() {
 // write your code here
+    digitalWrite(13, HIGH);
+    delay(1000);
+    digitalWrite(13, LOW);
+    delay(1000);
 }
